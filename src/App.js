0cfe4538c2c2e054e1311d38d7dbbf4a3f434fcc
Ik3nna <iamiykendukwu@gmail.com>
2022-10-14 10:50:47 +0100

    Styles and Theme Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./header";
 
 function App() {
   return(
     <>
-      
+      <Header />
     </>
   );
 }
--- src/header.js
+++ src/header.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Header() {
     return(
-        
+        <header>
+            <Container className="d-flex justify-content-center align-items-center">
+                <p>calc</p>
+
+                <div>
+                    <p>Theme</p>
+                </div>
+            </Container>
+        </header>
     );
 }
 
